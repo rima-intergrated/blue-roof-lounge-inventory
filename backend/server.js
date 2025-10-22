8fa7dfb879f1bbce465e3065f41da2298683eba1
Surface User <laisiedwin@gmail.com>
2025-10-22 15:31:20 +0200

    refactor(server): remove direct server listen log; prepare for improved logging

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,6 @@ require('dotenv').config({ path: path.join(__dirname, '.env') });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 // Middleware
 app.use(cors({
   origin: [
